6f58360012147f396f3a419237b834994db97c87
bymax00x <runalmanandhar@gmail.com>
2020-12-11 17:26:41 +0545

    addeed dashboard structure with css

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Header from "../Header/Header";
+import RightSidebar from "../RightSidebar";
 import "./Home.css";
 
 const Home = () => {
@@ -6,7 +8,17 @@ const Home = () => {
     <main>
       <section className="left">This is first</section>
 
-      <section className="right">This is right</section>
+      <section className="right">
+        <Header />
+        <div className="articles">
+          <article className="articles__cources">
+            <h1> Cources</h1>
+          </article>
+          <article className="articles__calender">
+            <RightSidebar />
+          </article>
+        </div>
+      </section>
     </main>
   );
 };
