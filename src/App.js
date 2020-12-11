896c252e2c9cdce156ad15369f69434c187c2af9
bymax00x <runalmanandhar@gmail.com>
2020-12-11 15:30:50 +0545

    dashboard structre createed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/">
-          <div>
-            <h1> This is redux react</h1>
-          </div>
+          <Home />
         </Route>
       </Switch>
     </Router>
---
+++ src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <main>
+      <section className="left">This is first</section>
+
+      <section className="right">This is right</section>
+    </main>
+  );
+};
+
+export default Home;
