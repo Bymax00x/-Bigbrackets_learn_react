a0934d47185ba2f0b5ea27529a1b4fd6eb3f6837
bymax00x <runalmanandhar@gmail.com>
2020-12-11 14:26:24 +0545

    added router
    react router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1> This is redux react</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <div>
+            <h1> This is redux react</h1>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
