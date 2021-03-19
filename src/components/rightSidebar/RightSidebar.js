d9514b0b0c8a7fac04cd43f3bf34fb36a74e10a6
timus-mahrez <timusansu777@gmail.com>
2021-03-19 20:40:16 +0545

    change in right side bar

--- src/components/rightSidebar/RightSidebar.js
+++ src/components/rightSidebar/RightSidebar.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import './Rightbar.css';
 
 const RightSidebar = () => {
 
@@ -11,12 +12,19 @@ const RightSidebar = () => {
   };
   return (
     <>
-    <div>
+    <div class="date_entry">
       <Calendar showWeekNumbers onChange ={onChange} value={date}/>
       {date.toString()}
     
     </div>
-    <div> <h1> Sumit Maharjan</h1></div>
+    <div class="style_h1"> 
+    <div class="style_h2">
+      <h1> We <span>Craft </span>Your <span>Imagination</span></h1>
+    </div>
+      <img src="big1.png"></img>
+  
+  
+    </div>
     </>
   );
 };
